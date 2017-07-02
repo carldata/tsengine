60d77581625a2c466d8b3d6a02a4d3cd7b137d5d
klangner <klangner@gmail.com>
2017-07-02 10:45:10 +0200

    Compiler mockup

---
+++ src/main/scala/com/carl/tsengine/compiler/Formater.scala
@@ -0,0 +1,13 @@
+package com.carl.tsengine.compiler
+
+import com.carl.tsengine.compiler.AST.Script
+
+/**
+  * Format AST to string
+  */
+object Formater {
+
+  def parse(script: Script): String = {
+    ???
+  }
+}
---
+++ src/main/scala/com/carl/tsengine/compiler/Parser.scala
@@ -0,0 +1,13 @@
+package com.carl.tsengine.compiler
+
+import com.carl.tsengine.compiler.AST.Script
+
+/**
+  * Script parser
+  */
+object Parser {
+
+  def parse(script: String): Script = {
+    ???
+  }
+}
