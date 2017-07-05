84377e03e5a3de03b553c7d3d42f81751747fe50
Krzysztof Langner <klangner@gmail.com>
2017-07-05 15:20:23 +0200

    Parser mockups

--- src/main/scala/com/carl/tsengine/compiler/FlowScript.g4
+++ src/main/scala/com/carl/tsengine/compiler/FlowScript.g4
@@ -1,21 +1,5 @@
 grammar FlowScript;
 
-// LEXER
-DEF : 'def';
-MODULE : 'module';
-// Whitespace and comments
-
-WS  :  [ \t\r\n\u000C]+ -> skip;
-LINE_COMMENT
-    :   '//' ~[\r\n]* -> skip
-    ;
-
-Identifier
-	: Letter LetterOrDigit*
-    ;
-
-
-// Parser
 /* Main compilation unit */
 compilationUnit
     : moduleDeclaration functionDeclaration EOF
@@ -50,3 +34,19 @@ fragment
 LetterOrDigit
 	: [a-zA-Z0-9$_] // these are the allowed characters or digits
     ;
+
+// LEXER
+DEF : 'def';
+MODULE : 'module';
+// Whitespace and comments
+
+WS  :  [ \t\r\n\u000C]+ -> skip;
+LINE_COMMENT
+    :   '//' ~[\r\n]* -> skip
+    ;
+
+Identifier
+	: Letter LetterOrDigit*
+    ;
+
+
--- src/main/scala/com/carl/tsengine/compiler/Parser.scala
+++ src/main/scala/com/carl/tsengine/compiler/Parser.scala
@@ -1,7 +1,8 @@
 package com.carl.tsengine.compiler
 
 import com.carl.tsengine.compiler.AST.{FunBody, FunDecl, Module, VarExpr}
-import org.antlr.v4.runtime.CharStreams
+import com.carl.tsengine.compiler.gen.{FlowScriptLexer, FlowScriptParser}
+import org.antlr.v4.runtime.{CharStreams, CommonTokenStream}
 
 /**
   * Script parser
@@ -10,6 +11,10 @@ object Parser {
 
   def parse(code: String): Module = {
     val input = CharStreams.fromString(code)
+    val lexer = new FlowScriptLexer(input)
+    val tokens = new CommonTokenStream(lexer)
+    val parser = new FlowScriptParser(tokens)
+    println(parser.compilationUnit())
     Module("Test1",
       FunDecl("my_fun", Seq("a", "xs"),
         FunBody(Seq(), VarExpr("a"))))
