bc7aae826c4797d04b81c5fc403160bb8863e11c
Krzysztof Langner <klangner@gmail.com>
2017-07-05 15:53:29 +0200

    Runtime mockup

---
+++ src/main/scala/com/carl/tsengine/runtime/Runtime.scala
@@ -0,0 +1,13 @@
+package com.carl.tsengine.runtime
+
+import com.carl.tsengine.compiler.AST.Module
+
+/**
+  * Run AST
+  */
+object Runtime {
+
+  def run(code: Module): Unit = {
+    println("Not implemented yet!")
+  }
+}
