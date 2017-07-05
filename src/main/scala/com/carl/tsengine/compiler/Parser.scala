f5f9c5087b079a1eff29cf9578f8534d0a5b6b11
Krzysztof Langner <klangner@gmail.com>
2017-07-05 14:01:09 +0200

    fixed Travis build

--- src/main/scala/com/carl/tsengine/compiler/Parser.scala
+++ src/main/scala/com/carl/tsengine/compiler/Parser.scala
@@ -1,13 +1,17 @@
 package com.carl.tsengine.compiler
 
-import com.carl.tsengine.compiler.AST.{FunExpr, ScriptAST}
+import com.carl.tsengine.compiler.AST.{FunBody, FunDecl, Module, VarExpr}
+import org.antlr.v4.runtime.CharStreams
 
 /**
   * Script parser
   */
 object Parser {
 
-  def parse(code: String): ScriptAST = {
-    ScriptAST(Seq(), FunExpr("empty_series", Seq()))
+  def parse(code: String): Module = {
+    val input = CharStreams.fromString(code)
+    Module("Test1",
+      FunDecl("my_fun", Seq("a", "xs"),
+        FunBody(Seq(), VarExpr("a"))))
   }
 }
