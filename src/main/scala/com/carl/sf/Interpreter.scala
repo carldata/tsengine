f536bd8aa5a3113f1fb4245b3c74de91138deda2
klangner <klangner@gmail.com>
2017-07-09 09:48:52 +0200

    Implemented interpreter for grammar ver. 0.1

--- src/main/scala/com/carl/sf/Interpreter.scala
+++ src/main/scala/com/carl/sf/Interpreter.scala
@@ -1,6 +1,9 @@
 package com.carl.sf
 
-import com.carl.sf.compiler.AST.Module
+import com.carl.sf.compiler.AST
+import com.carl.sf.compiler.AST.{FunctionDef, Module, Term, VariableTerm}
+
+import scala.collection.mutable
 
 
 /**
@@ -21,9 +24,19 @@ object Interpreter {
     */
   def run(module: Module, params: Seq[Value]): Either[String, Value] = {
     if(params.size == module.funDecl.params.size) {
-      Right(IntValue(params.size))
+      val symbolMemory = module.funDecl.params.zip(params).map(x => x._1 -> x._2).toMap
+      execExpr(module.funDecl.body, symbolMemory)
     } else {
-      Left("Not implemented yet!")
+      Left("Wrong number of parameters")
     }
   }
+
+  /** Execute node with the function declaration */
+  def execExpr(expr: Term, symbolMemory: Map[String, Value]): Either[String, Value] = {
+    expr match {
+      case VariableTerm(name) => Right(symbolMemory.getOrElse(name, UnitValue))
+    }
+  }
+
+
 }
--- src/test/scala/com/carl/sf/InterpreterTest.scala
+++ src/test/scala/com/carl/sf/InterpreterTest.scala
@@ -35,16 +35,16 @@ class InterpreterTest extends FlatSpec with Matchers {
     result.isRight shouldBe true
   }
 
-  //  "Runtime" should "return value of the parameter" in {
-//    val code =
-//      """
-//        |module Test1
-//        |
-//        |def main(a, xs) = a
-//      """.stripMargin
-//    val result = Compiler.compile(code).flatMap { ast =>
-//      Runtime.run(ast, Seq(IntValue(1), IntValue(2)))
-//    }
-//    result shouldBe Right(IntValue(2))
-//  }
+    it should "return value of the parameter" in {
+    val code =
+      """
+        |module Test1
+        |
+        |def main(a, xs) = a
+      """.stripMargin
+    val result = Compiler.compile(code).flatMap { ast =>
+      Interpreter.run(ast, Seq(IntValue(1), IntValue(2)))
+    }
+    result shouldBe Right(IntValue(1))
+  }
 }
