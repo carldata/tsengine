682b7cb0191f08fb8fff0218b1a2c9492335881b
Krzysztof Langner <klangner@gmail.com>
2017-07-24 09:30:47 +0200

    Added pow, exp and sqrt functions

--- src/main/scala/com/carl/sf/core/Core.scala
+++ src/main/scala/com/carl/sf/core/Core.scala
@@ -3,8 +3,6 @@ package com.carl.sf.core
 import com.carl.sf.Runtime
 import com.carl.sf.Runtime.NumberValue
 
-import scala.math._
-
 /**
   * Core functions and types which can be accessed from the script
   */
@@ -14,16 +12,22 @@ object Core {
     """
       |module Core
       |
+      |external def exp(a: Number): Number
       |external def max(a: Number, b: Number): Number
       |external def min(a: Number, b: Number): Number
+      |external def pow(a: Number, b: Number): Number
+      |external def sqrt(a: Number): Number
     """.stripMargin
 }
 
 class Core extends Runtime{
 
-
   // Function definition
-  def $max(a: NumberValue, b: NumberValue): NumberValue = NumberValue(max(a.v, b.v))
-  def $min(a: NumberValue, b: NumberValue): NumberValue = NumberValue(min(a.v, b.v))
+  def $exp(a: NumberValue): NumberValue = NumberValue(math.exp(a.v.toDouble).toFloat)
+  def $max(a: NumberValue, b: NumberValue): NumberValue = NumberValue(math.max(a.v, b.v))
+  def $min(a: NumberValue, b: NumberValue): NumberValue = NumberValue(math.min(a.v, b.v))
+  def $pow(a: NumberValue, b: NumberValue): NumberValue =
+    NumberValue(math.pow(a.v.toDouble, b.v.toDouble).toFloat)
+  def $sqrt(a: NumberValue): NumberValue = NumberValue(math.sqrt(a.v.toDouble).toFloat)
 }
 
