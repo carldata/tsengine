d64f6bbe40059fecdb5730b2e3a6da7277f9bce3
Krzysztof Langner <klangner@gmail.com>
2017-08-02 11:41:45 +0200

    Implemented rounding functions Fixed #41

--- src/main/scala/carldata/sf/Core.scala
+++ src/main/scala/carldata/sf/Core.scala
@@ -11,14 +11,17 @@ object Core {
     """
       |module Core
       |
+      |external def ceil(a: Number): Number
       |external def cos(a: Number): Number
       |external def exp(a: Number): Number
+      |external def floor(a: Number): Number
       |external def log(a: Number): Number
       |external def log10(a: Number): Number
       |external def max(a: Number, b: Number): Number
       |external def min(a: Number, b: Number): Number
       |external def pi(): Number
       |external def pow(a: Number, b: Number): Number
+      |external def round(a: Number): Number
       |external def sin(a: Number): Number
       |external def sqrt(a: Number): Number
       |external def tan(a: Number): Number
@@ -29,8 +32,10 @@ object Core {
 class Core extends Runtime{
 
   // Function definition
+  def $ceil(a: NumberValue): NumberValue = NumberValue(math.ceil(a.v.toDouble).toFloat)
   def $cos(a: NumberValue): NumberValue = NumberValue(math.cos(a.v.toDouble).toFloat)
   def $exp(a: NumberValue): NumberValue = NumberValue(math.exp(a.v.toDouble).toFloat)
+  def $floor(a: NumberValue): NumberValue = NumberValue(math.floor(a.v.toDouble).toFloat)
   def $log(a: NumberValue): NumberValue = NumberValue(math.log(a.v.toDouble).toFloat)
   def $log10(a: NumberValue): NumberValue = NumberValue(math.log10(a.v.toDouble).toFloat)
   def $max(a: NumberValue, b: NumberValue): NumberValue = NumberValue(math.max(a.v, b.v))
@@ -38,6 +43,7 @@ class Core extends Runtime{
   def $pi(): NumberValue = NumberValue(math.Pi.toFloat)
   def $pow(a: NumberValue, b: NumberValue): NumberValue =
     NumberValue(math.pow(a.v.toDouble, b.v.toDouble).toFloat)
+  def $round(a: NumberValue): NumberValue = NumberValue(math.round(a.v))
   def $sin(a: NumberValue): NumberValue = NumberValue(math.sin(a.v.toDouble).toFloat)
   def $sqrt(a: NumberValue): NumberValue = NumberValue(math.sqrt(a.v.toDouble).toFloat)
   def $tan(a: NumberValue): NumberValue = NumberValue(math.tan(a.v.toDouble).toFloat)
