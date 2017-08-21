af9caa0bd946e9d812fa5d5177685da249f2115c
wenta <jakub@carlsolutions.com>
2017-08-21 16:15:59 +0200

    Update tests and fix for Runtime

--- src/main/scala/carldata/sf/core/DateTime.scala
+++ src/main/scala/carldata/sf/core/DateTime.scala
@@ -12,15 +12,15 @@ object DateTime {
   // Header which will be provided to the compiler
   val header: String =
     """
-      |external def $adjust_date(dt: DateTime, y: Number, m: Number, d: Number): DateTime
-      |external def $adjust_time(dt: DateTime, h: Number, m: Number, s: Number): DateTime
+      |external def adjust_date(dt: DateTime, y: Number, m: Number, d: Number): DateTime
+      |external def adjust_time(dt: DateTime, h: Number, m: Number, s: Number): DateTime
       |external def date(d: String): DateTime
-      |external def datetime(y: Number, m: Number, d: Number): DateTime
-      |external def datetime(y: Number, m: Number, d: Number, h: Number, mt: Number, s: Number, ns: Number): DateTime
+      |external def from_date(y: Number, m: Number, d: Number): DateTime
+      |external def from_datetime(y: Number, m: Number, d: Number, h: Number, mt: Number, s: Number, ns: Number): DateTime
       |external def day_of_week(dt: DateTime) : Number
-      |external def $floor_hours(dt: DateTime): DateTime
-      |external def $floor_minutes(dt: DateTime): DateTime
-      |external def $floor_seconds(dt: DateTime): DateTime
+      |external def floor_hours(dt: DateTime): DateTime
+      |external def floor_minutes(dt: DateTime): DateTime
+      |external def floor_seconds(dt: DateTime): DateTime
     """.stripMargin
 }
 
@@ -32,8 +32,8 @@ class DateTime extends Runtime {
   def $adjust_date(dt: DateTimeValue, y: NumberValue, m: NumberValue, d: NumberValue): DateTimeValue = DateTimeValue(dt.dt.withYear(parse(y)).withMonth(parse(m)).withDayOfMonth(parse(d)))
   def $adjust_time(dt: DateTimeValue, h: NumberValue, m: NumberValue, s: NumberValue): DateTimeValue = DateTimeValue(dt.dt.withHour(parse(h)).withMinute(parse(m)).withSecond(parse(m)))
   def $date(s: StringValue): DateTimeValue = DateTimeValue(LocalDateTime.parse(s.str))
-  def $datetime(y: NumberValue, m: NumberValue, d: NumberValue): DateTimeValue = DateTimeValue(LocalDateTime.of(parse(y), parse(m), parse(d), 0, 0, 0))
-  def $datetime(y: NumberValue, m: NumberValue, d: NumberValue, h: NumberValue, mt: NumberValue, s: NumberValue, ns: NumberValue): DateTimeValue = DateTimeValue(LocalDateTime.of(parse(y), parse(m), parse(d), parse(h), parse(mt), parse(s), parse(ns)))
+  def $from_date(y: NumberValue, m: NumberValue, d: NumberValue): DateTimeValue = DateTimeValue(LocalDateTime.of(parse(y), parse(m), parse(d), 0, 0, 0))
+  def $from_datetime(y: NumberValue, m: NumberValue, d: NumberValue, h: NumberValue, mt: NumberValue, s: NumberValue, ns: NumberValue): DateTimeValue = DateTimeValue(LocalDateTime.of(parse(y), parse(m), parse(d), parse(h), parse(mt), parse(s), parse(ns)))
   def $day_of_week(dt: DateTimeValue) : NumberValue = NumberValue(dt.dt.getDayOfWeek.getValue)
   def $floor_hours(dt: DateTimeValue): DateTimeValue = DateTimeValue(dt.dt.withHour(0))
   def $floor_minutes(dt: DateTimeValue): DateTimeValue = DateTimeValue(dt.dt.withMinute(0))
--- src/test/scala/carldata/sf/core/DateTimeTest.scala
+++ src/test/scala/carldata/sf/core/DateTimeTest.scala
@@ -1,54 +1,112 @@
-package carldata.sf.core
+package carldata.sf
 
 import carldata.sf.Runtime.{NumberValue, StringValue}
+import carldata.sf.core.DateTime
 import org.scalatest._
 
 class DateTimeTest extends FlatSpec with Matchers {
-  private val libs = Seq(new Math())
+  val libs = Seq(DateTime.header)
+  val dt = new DateTime()
 
   "Datetime" should "convert date from string" in {
-    val str = "2017-08-21T10:30:30"
-    val dt = new DateTime()
-    dt.$date(StringValue(str)) shouldBe dt.$datetime(NumberValue(2017), NumberValue(8), NumberValue(21), NumberValue(10), NumberValue(30), NumberValue(30), NumberValue(0))
+    val code =
+      """
+        |def main(s: String): DateTime = date(s)
+      """.stripMargin
+
+    val params = Seq(new StringValue("2017-08-21T10:30:30"))
+    val result = Compiler.compile(code, libs).flatMap { exec =>
+      Interpreter(exec).run("main", params)
+    }
+    result.right.get.toString shouldEqual dt.$from_datetime(NumberValue(2017), NumberValue(8), NumberValue(21), NumberValue(10), NumberValue(30), NumberValue(30), NumberValue(0)).toString
   }
 
   it should "adjust date" in {
-    val dt = new DateTime()
-    val dt1 = dt.$datetime(NumberValue(2017), NumberValue(8), NumberValue(21))
-    dt.$adjust_date(dt1,NumberValue(2016), NumberValue(1), NumberValue(1)) shouldBe dt.$datetime(NumberValue(2016), NumberValue(1), NumberValue(1))
+    val code =
+      """
+        |def main(dt: DateTime,y: Number, m: Number, d:Number): DateTime = adjust_date(dt,y,m,d)
+      """.stripMargin
+
+    val params = Seq(dt.$from_date(NumberValue(2017), NumberValue(8), NumberValue(21)), NumberValue(2016), NumberValue(1), NumberValue(1))
+    val result = Compiler.compile(code, libs).flatMap { exec =>
+      Interpreter(exec).run("main", params)
+    }
+    result.right.get.toString shouldBe dt.$from_date(NumberValue(2016), NumberValue(1), NumberValue(1)).toString
   }
 
   it should "adjust time" in {
-    val dt = new DateTime()
-    val dt1 = dt.$datetime(NumberValue(2017), NumberValue(8), NumberValue(21))
-    dt.$adjust_time(dt1,NumberValue(10), NumberValue(30), NumberValue(30)) shouldBe dt.$datetime(NumberValue(2017), NumberValue(8), NumberValue(21), NumberValue(10), NumberValue(30), NumberValue(30), NumberValue(0))
+    val code =
+      """
+        |def main(dt: DateTime,y: Number, m: Number, d:Number): DateTime = adjust_time(dt,y,m,d)
+      """.stripMargin
+
+    val params = Seq(dt.$from_date(NumberValue(2017), NumberValue(8), NumberValue(21)), NumberValue(10), NumberValue(30), NumberValue(30))
+    val result = Compiler.compile(code, libs).flatMap { exec =>
+      Interpreter(exec).run("main", params)
+    }
+    result.right.get.toString shouldBe dt.$from_datetime(NumberValue(2017), NumberValue(8), NumberValue(21), NumberValue(10), NumberValue(30), NumberValue(30), NumberValue(0)).toString
   }
+
   it should "get day of week" in {
-    val dt = new DateTime()
-    val dt1 = dt.$datetime(NumberValue(2017), NumberValue(8), NumberValue(21)) // Monday
-    val dt2 = dt.$datetime(NumberValue(2017), NumberValue(8), NumberValue(23)) // Wednesday
-    val dt3 = dt.$datetime(NumberValue(2017), NumberValue(8), NumberValue(26)) // Saturday
-    dt.$day_of_week(dt1) shouldBe NumberValue(1)
-    dt.$day_of_week(dt2) shouldBe NumberValue(3)
-    dt.$day_of_week(dt3) shouldBe NumberValue(6)
+    val code =
+      """
+        |def main(dt: DateTime): Number = day_of_week(dt)
+      """.stripMargin
+
+    val result1 = Compiler.compile(code, libs).flatMap { exec =>
+      Interpreter(exec).run("main", Seq(dt.$from_date(NumberValue(2017), NumberValue(8), NumberValue(21))))
+    }
+    result1.right.get shouldBe NumberValue(1)
+
+    val result2 = Compiler.compile(code, libs).flatMap { exec =>
+      Interpreter(exec).run("main", Seq(dt.$from_date(NumberValue(2017), NumberValue(8), NumberValue(23))))
+    }
+    result2.right.get shouldBe NumberValue(3)
+
+    val result3 = Compiler.compile(code, libs).flatMap { exec =>
+      Interpreter(exec).run("main", Seq(dt.$from_date(NumberValue(2017), NumberValue(8), NumberValue(26))))
+    }
+    result3.right.get shouldBe NumberValue(6)
+
   }
 
   it should "floor hours" in {
-    val dt = new DateTime()
-    val dt1 = dt.$datetime(NumberValue(2017), NumberValue(8), NumberValue(21), NumberValue(11), NumberValue(31), NumberValue(36), NumberValue(0))
-    dt.$floor_hours(dt1) shouldBe dt.$datetime(NumberValue(2017), NumberValue(8), NumberValue(21), NumberValue(0), NumberValue(31), NumberValue(36), NumberValue(0))
+    val code =
+      """
+        |def main(dt: DateTime): DateTime = floor_hours(dt)
+      """.stripMargin
+
+    val params = Seq(dt.$from_datetime(NumberValue(2017), NumberValue(8), NumberValue(21), NumberValue(11), NumberValue(31), NumberValue(36), NumberValue(0)))
+    val result = Compiler.compile(code, libs).flatMap { exec =>
+      Interpreter(exec).run("main", params)
+    }
+    result.right.get.toString shouldBe dt.$from_datetime(NumberValue(2017), NumberValue(8), NumberValue(21), NumberValue(0), NumberValue(31), NumberValue(36), NumberValue(0)).toString
   }
 
   it should "floor minutes" in {
-    val dt = new DateTime()
-    val dt1 = dt.$datetime(NumberValue(2017), NumberValue(8), NumberValue(21), NumberValue(11), NumberValue(31), NumberValue(36), NumberValue(0))
-    dt.$floor_minutes(dt1) shouldBe dt.$datetime(NumberValue(2017), NumberValue(8), NumberValue(21), NumberValue(11), NumberValue(0), NumberValue(36), NumberValue(0))
+    val code =
+      """
+        |def main(dt: DateTime): DateTime = floor_minutes(dt)
+      """.stripMargin
+
+    val params = Seq(dt.$from_datetime(NumberValue(2017), NumberValue(8), NumberValue(21), NumberValue(11), NumberValue(31), NumberValue(36), NumberValue(0)))
+    val result = Compiler.compile(code, libs).flatMap { exec =>
+      Interpreter(exec).run("main", params)
+    }
+    result.right.get.toString shouldBe dt.$from_datetime(NumberValue(2017), NumberValue(8), NumberValue(21), NumberValue(11), NumberValue(0), NumberValue(36), NumberValue(0)).toString
   }
 
   it should "floor seconds" in {
-    val dt = new DateTime()
-    val dt1 = dt.$datetime(NumberValue(2017), NumberValue(8), NumberValue(21), NumberValue(11), NumberValue(31), NumberValue(36), NumberValue(0))
-    dt.$floor_seconds(dt1) shouldBe dt.$datetime(NumberValue(2017), NumberValue(8), NumberValue(21), NumberValue(11), NumberValue(31), NumberValue(0), NumberValue(0))
+    val code =
+      """
+        |def main(dt: DateTime): DateTime = floor_seconds(dt)
+      """.stripMargin
+
+    val params = Seq(dt.$from_datetime(NumberValue(2017), NumberValue(8), NumberValue(21), NumberValue(11), NumberValue(31), NumberValue(36), NumberValue(0)))
+    val result = Compiler.compile(code, libs).flatMap { exec =>
+      Interpreter(exec).run("main", params)
+    }
+    result.right.get.toString shouldBe dt.$from_datetime(NumberValue(2017), NumberValue(8), NumberValue(21), NumberValue(11), NumberValue(31), NumberValue(0), NumberValue(0)).toString
   }
 
 }
