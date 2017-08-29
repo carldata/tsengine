649f6729b90d507d85eb28759b97f4d35dab60b4
Krzysztof Langner <klangner@gmail.com>
2017-08-29 15:33:02 +0200

    Refactored benchmarks

--- src/main/scala/carldata/sf/BenchmarkApp.scala
+++ src/main/scala/carldata/sf/BenchmarkApp.scala
@@ -9,44 +9,37 @@ import org.scalameter._
   */
 object BenchmarkApp {
 
+  val mapScript: String =
+    """
+      |def f(a: Number): Number = a+2
+      |
+      |def main(xs: TimeSeries): TimeSeries = map(xs, f)
+    """.stripMargin
+  val deltaTimeScript = "def main(xs: TimeSeries): TimeSeries = delta_time(xs)"
+
+  private val intFormatter = java.text.NumberFormat.getIntegerInstance
+
   def main(args: Array[String]): Unit = {
-    measureMap()
+    val size1M = 1000000
+
+    println("\n1. Measure map")
+    measureScript(size1M, mapScript)
+    println("\n1. Measure delta_time")
+    measureScript(size1M, deltaTimeScript)
+    println()
   }
 
-  def measureMap(): Unit = {
-    val code =
-      """
-        |def f(a: Number): Number = a+2
-        |
-        |def main(xs: TimeSeries): TimeSeries = map(xs, f)
-      """.stripMargin
-    val xs = 1.to(1000000).toVector
-    val ts = TimeSeries.fromTimestamps(xs.map(x => (x.toLong, x.toFloat)))
-    val exec = Compiler.make(code)
-      .map { ast => Interpreter(ast) }
-      .right.get
+  def measureScript(size: Int, code: String): Unit = {
+    val xs = 1.to(size).toVector
+    val ts = TimeSeries.fromTimestamps(xs.map(x => (x.toLong*3000, x.toFloat)))
+    val exec = Compiler.make(code).map { ast => Interpreter(ast) }.right.get
 
-    val timeNative: Quantity[Double] = withWarmer(new Warmer.Default).measure {
-      ts.map(x => x._2 + 2)
-    }
-
-    val timeScript: Quantity[Double] = withWarmer(new Warmer.Default).measure {
+    val time: Quantity[Double] = withWarmer(new Warmer.Default).measure {
       exec.run("main", Seq(ts))
     }
 
-    println(s"map over 1M points (native: $timeNative, script: $timeScript)")
-  }
-
-  /** Some sandbox for testing scripts */
-  def sandbox(): Unit = {
-    val code =
-      """
-        |def main(a: Number, b: Number): Number = a+b
-      """.stripMargin
-    val result = Compiler.compile(code, Seq()).flatMap { ast =>
-      Interpreter(ast).run("main", Seq(2, 2))
-    }
-    println(result)
+    val sizeFormatted = intFormatter.format(size)
+    println(s"$sizeFormatted points: $time.")
   }
 
 }
