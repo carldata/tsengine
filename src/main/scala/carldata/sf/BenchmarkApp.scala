5ea19d19a3be3e9f3729cbd3841ce4f476cae536
wenta <jakubwenta@gmail.com>
2017-09-19 09:44:24 +0200

    Update benchmark for lookup.

--- src/main/scala/carldata/sf/BenchmarkApp.scala
+++ src/main/scala/carldata/sf/BenchmarkApp.scala
@@ -18,8 +18,12 @@ object BenchmarkApp {
     """.stripMargin
   val deltaTimeScript = "def main(xs: TimeSeries): TimeSeries = delta_time(xs)"
 
-  val lookupScript = "def main(id: String, x: Number): Number = lookup(id, x)"
-
+  val lookupScript =
+    """
+      |def f(a: Number): Number = lookup("test", a)
+      |
+      |def main(xs: TimeSeries): TimeSeries = map(xs, f)
+    """.stripMargin
   private val intFormatter = java.text.NumberFormat.getIntegerInstance
 
   def main(args: Array[String]): Unit = {
@@ -30,15 +34,15 @@ object BenchmarkApp {
     println("\n2. Measure delta_time")
     measureScript(size1M, deltaTimeScript)
     println("\n3. Measure lookup")
-    measureScript(size1M, deltaTimeScript)
+    measureScript(size1M, lookupScript)
     println()
   }
 
   def measureScript(size: Int, code: String): Unit = {
     val xs = 1.to(size).toVector
-    val test_db: TestDB = new TestDB
+    val testDB: TestDB = new TestDB
     val ts = TimeSeries.fromTimestamps(xs.map(x => (x.toLong * 3000, x.toFloat)))
-    val exec = Compiler.make(code).map { ast => Interpreter(ast, test_db) }.right.get
+    val exec = Compiler.make(code).map { ast => Interpreter(ast, testDB) }.right.get
 
     val time: Quantity[Double] = withWarmer(new Warmer.Default).measure {
       exec.run("main", Seq(ts))
@@ -51,7 +55,9 @@ object BenchmarkApp {
 }
 
 class TestDB extends DBImplementation {
-  val lookup_table = Seq(("test", 1f, 1f))
+  def f(x: Float): (String, Float, Float) = ("test", x, 2 * x)
+
+  val lookup_table: Seq[(String, Float, Float)] = for (x <- 1 to 10000) yield f(x)
 
   def getTable(id: String): IndexedSeq[(Float, Float)] = {
     lookup_table.filter(p => p._1.equals(id)).map(x => (x._2, x._3)).toIndexedSeq
