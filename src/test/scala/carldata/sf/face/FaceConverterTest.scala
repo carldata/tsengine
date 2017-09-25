ae6d7eeab8fed0332c1053968809887e0dc7da46
wenta <jakubwenta@gmail.com>
2017-09-25 13:56:48 +0200

    Fixed #97 (#98)

--- src/test/scala/carldata/sf/face/FaceConverterTest.scala
+++ src/test/scala/carldata/sf/face/FaceConverterTest.scala
@@ -19,11 +19,11 @@ class FaceConverterTest extends FlatSpec with Matchers {
 
     val faceAST = FaceParser.parse(face).right.get
     val expected = Parser.parse(flowScript).right.get
-    val result = FaceConverter.convert(faceAST)
+    val result = FaceConverter.convert(faceAST).right.get
     result shouldBe expected
   }
 
-  "FACE Converter" should "convert complex expression" in {
+  it should "convert expression with 2 variables" in {
     val face = "2 * a + 4 / b"
     val flowScript =
       """
@@ -33,8 +33,49 @@ class FaceConverterTest extends FlatSpec with Matchers {
 
     val faceAST = FaceParser.parse(face).right.get
     val expected = Parser.parse(flowScript).right.get
-    val result = FaceConverter.convert(faceAST)
+    val result = FaceConverter.convert(faceAST).right.get
     result shouldBe expected
+  }
+
+  it should "convert expression with 3 variables" in {
+    val face = "a + b * c"
+    val flowScript =
+      """
+        |def f(a: Number, b: Number, c: Number): Number = a + b * c
+        |def main(a: TimeSeries, b: TimeSeries, c: TimeSeries): TimeSeries = join_with3(a, b, c, f)
+      """.stripMargin
+
+    val faceAST = FaceParser.parse(face).right.get
+    val expected = Parser.parse(flowScript).right.get
+    val result = FaceConverter.convert(faceAST).right.get
+    result shouldBe expected
+  }
+
+  it should "convert expression with 4 variables" in {
+    val face = "a + b + c * d"
+    val flowScript =
+      """
+        |def f(a: Number, b: Number, c: Number, d: Number): Number = a + b + c * d
+        |def main(a: TimeSeries, b: TimeSeries, c: TimeSeries, d: TimeSeries): TimeSeries = join_with4(a, b, c, d, f)
+      """.stripMargin
+
+    val faceAST = FaceParser.parse(face).right.get
+    val expected = Parser.parse(flowScript).right.get
+    val result = FaceConverter.convert(faceAST).right.get
+    result shouldBe expected
+  }
+
+  it should "give error message on too much od params" in {
+    val face = "2 * a + 4 / b + c + d + e"
+    val flowScript =
+      """
+        |def f(a: Number, b: Number): Number = 2 * a + 4 / b
+        |def main(a: TimeSeries, b: TimeSeries): TimeSeries = join_with(a, b, f)
+      """.stripMargin
+
+    val faceAST = FaceParser.parse(face).right.get
+    FaceConverter.convert(faceAST).isLeft
+
   }
 
 }
