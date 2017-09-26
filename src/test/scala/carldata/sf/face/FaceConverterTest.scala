456c32a18d1f49a0245c34e15f729f539125f179
wenta <jakubwenta@gmail.com>
2017-09-26 14:25:50 +0200

    Issue #100.2 (#105)
    * Cover boolean operations in converter + test.
    
    * Fix test for simple function.
    
    * Support for negation in converter + test.

--- src/test/scala/carldata/sf/face/FaceConverterTest.scala
+++ src/test/scala/carldata/sf/face/FaceConverterTest.scala
@@ -87,7 +87,23 @@ class FaceConverterTest extends FlatSpec with Matchers {
       """.stripMargin
 
     val faceAST = FaceParser.parse(face).right.get
-    FaceConverter.convert(faceAST).isLeft
+    val expected = Parser.parse(flowScript).right.get
+    val result = FaceConverter.convert(faceAST).right.get
+    result shouldBe expected
+
+  }
+
+  it should "convert nested functions" in {
+    val face = "h(g(a-2.41),1)"
+    val flowScript =
+      """
+        |def f(a: Number): Number = h(g(a-2.41),1)
+        |def main(a: TimeSeries): TimeSeries = map(a, f)
+      """.stripMargin
+    val faceAST = FaceParser.parse(face).right.get
+    val result = FaceConverter.convert(faceAST).right.get
+    val expected = Parser.parse(flowScript).right.get
+    result shouldBe expected
 
   }
 
@@ -96,6 +112,34 @@ class FaceConverterTest extends FlatSpec with Matchers {
     val flowScript =
       """
         |def f(a: Number): Number = if a > 10 then 5 else 0
+        |def main(a: TimeSeries): TimeSeries = map(a, f)
+      """.stripMargin
+
+    val faceAST = FaceParser.parse(face).right.get
+    val expected = Parser.parse(flowScript).right.get
+    val result = FaceConverter.convert(faceAST).right.get
+    result shouldBe expected
+  }
+
+  it should "convert nested if expression" in {
+    val face = "IF(a > 10 && 0 < 1,5,0)"
+    val flowScript =
+      """
+        |def f(a: Number): Number = if a > 10 && 0 < 1 then 5 else 0
+        |def main(a: TimeSeries): TimeSeries = map(a, f)
+      """.stripMargin
+
+    val faceAST = FaceParser.parse(face).right.get
+    val expected = Parser.parse(flowScript).right.get
+    val result = FaceConverter.convert(faceAST).right.get
+    result shouldBe expected
+  }
+
+  it should "convert expression with negation" in {
+    val face = "if(!(a < 1),0,1)"
+    val flowScript =
+      """
+        |def f(a: Number): Number = if !a  < 1 then 0 else 1
         |def main(a: TimeSeries): TimeSeries = map(a, f)
       """.stripMargin
 
