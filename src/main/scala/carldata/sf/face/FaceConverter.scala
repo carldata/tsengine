cc8742b714cb282d191628eb7fc2d09a9d8a5054
wenta <jakubwenta@gmail.com>
2017-12-20 11:10:30 +0100

    Refactor Face Converter to use arithmetic operations on Time Series F… (#123)
    * Refactor Face Converter to use arithmetic operations on Time Series Fixed #122
    
    * Update IF statement converter.
    
    * Fix for numbers.

--- src/main/scala/carldata/sf/face/FaceConverter.scala
+++ src/main/scala/carldata/sf/face/FaceConverter.scala
@@ -8,11 +8,12 @@ import carldata.sf.compiler.AST._
 object FaceConverter {
 
   def convert(face: Expression): Either[String, Module] = {
-    val vs = if(face.isInstanceOf[NumberLiteral]) Set("x") else freeVariable(face)
+    val vs = if (face.isInstanceOf[NumberLiteral]) Set("x") else freeVariable(face)
     val expr = convertIf(face)
-    val main = mkMain(vs)
-    main.map(f => Module(Seq(), Seq(mkFunction(expr, vs), f)))
+    val main = mkMain(expr, vs)
+    mkModule(expr, main, vs)
   }
+
 
   def freeVariable(e: Expression): Set[String] = {
     e match {
@@ -20,7 +21,7 @@ object FaceConverter {
       case b: BinaryOpExpr => freeVariable(b.e1) ++ freeVariable(b.e2)
       case l: BoolOpExpr => freeVariable(l.e1) ++ freeVariable(l.e2)
       case r: RelationExpr => freeVariable(r.e1) ++ freeVariable(r.e2)
-      case a: AppExpr => a.params.flatMap(x=>freeVariable(x)).toSet
+      case a: AppExpr => a.params.flatMap(x => freeVariable(x)).toSet
       case n: NegOpExpr => freeVariable(n.expr)
       case _ => Set()
     }
@@ -30,9 +31,18 @@ object FaceConverter {
     expr match {
       case b: BinaryOpExpr => BinaryOpExpr(convertIf(b.e1), b.op, convertIf(b.e2))
       case a: AppExpr =>
-        if(a.name.toLowerCase == "if" && a.params.size == 3) IfExpr(a.params.head, a.params(1), a.params(2))
+        if (a.name.toLowerCase == "if" && a.params.size == 3) IfExpr(a.params.head, a.params(1), a.params(2))
         else AppExpr(a.name, a.params.map(convertIf))
       case e => e
+    }
+  }
+
+  def mkModule(expr: Expression, main: Either[String, FunctionDef], vs: Set[String]): Either[String, Module] = {
+    expr match {
+      case AppExpr(_, _) => main.map(f => Module(Seq(), Seq(mkFunction(expr, vs), f)))
+      case IfExpr(_, _, _) => main.map(f => Module(Seq(), Seq(mkFunction(expr, vs), f)))
+      case NumberLiteral(_) => main.map(f => Module(Seq(), Seq(mkFunction(expr, vs), f)))
+      case _ => main.map(f => Module(Seq(), Seq(f)))
     }
   }
 
@@ -40,16 +50,16 @@ object FaceConverter {
     FunctionDef("f", s.map(x => FunParam(x, ValueType("Number"))).toList, ValueType("Number"), FunctionBody(Seq.empty, e))
   }
 
-  def mkMain(s: Set[String]): Either[String, FunctionDef] = {
-    if (s.isEmpty || s.size > 4) {
+  def mkMain(expr: Expression, s: Set[String]): Either[String, FunctionDef] = {
+    if (s.isEmpty) {
       Left("Wrong number of parameters")
     }
     else {
-      val e: Expression = s.size match {
-        case 1 => AppExpr("map", List(VariableExpr(s.head), VariableExpr("f")))
-        case 2 => AppExpr("join_with", s.map(x => VariableExpr(x)).toList :+ VariableExpr("f"))
-        case 3 => AppExpr("join_with3", s.map(x => VariableExpr(x)).toList :+ VariableExpr("f"))
-        case 4 => AppExpr("join_with4", s.map(x => VariableExpr(x)).toList :+ VariableExpr("f"))
+      val e: Expression = expr match {
+        case AppExpr(_, _) => AppExpr("map", List(VariableExpr(s.head), VariableExpr("f")))
+        case IfExpr(_, _, _) => AppExpr("map", List(VariableExpr(s.head), VariableExpr("f")))
+        case NumberLiteral(_) => AppExpr("map", List(VariableExpr(s.head), VariableExpr("f")))
+        case _ => expr
       }
       Right(FunctionDef("main", s.map(x => FunParam(x, ValueType("TimeSeries"))).toList, ValueType("TimeSeries"), FunctionBody(Seq.empty, e)))
     }
