fb612a4a8a6f293c4a9633b768d83be598358c8b
wenta <jakubwenta@gmail.com>
2018-01-15 11:37:50 +0100

    Remove boolean from shift function. Fixed #141 (#142)
    * Remove boolean from shift function. Fixed #141
    
    * Refactoring.

--- src/main/scala/carldata/sf/core/TimeSeriesModule.scala
+++ src/main/scala/carldata/sf/core/TimeSeriesModule.scala
@@ -31,7 +31,7 @@ object TimeSeriesModule {
       |external def rolling_avg(xs: TimeSeries, d: Duration): TimeSeries
       |external def rolling_sum(xs: TimeSeries, d: Duration): TimeSeries
       |external def running_total(xs: TimeSeries, f: DateTime => DateTime): TimeSeries
-      |external def shift(xs: TimeSeries, d: Duration, f: Boolean): TimeSeries
+      |external def shift(xs: TimeSeries, d: Duration): TimeSeries
       |external def slice(xs: TimeSeries, sd: DateTime, ed: DateTime): TimeSeries
       |external def step(xs: TimeSeries, d: Duration): TimeSeries
       |external def time_weight_average(xs: TimeSeries, d: Duration): TimeSeries
@@ -123,7 +123,7 @@ class TimeSeriesModule extends Runtime {
 
   def $running_total(xs: TimeSeries[Float], f: LocalDateTime => LocalDateTime): TimeSeries[Float] = TimeSeries.integrateByTime(xs, f)
 
-  def $shift(xs: TimeSeries[Float], d: Duration, f: Boolean): TimeSeries[Float] = xs.shiftTime(d, f)
+  def $shift(xs: TimeSeries[Float], d: Duration): TimeSeries[Float] = xs.shiftTime(d, forward = true)
 
   def $slice(xs: TimeSeries[Float], sd: LocalDateTime, ed: LocalDateTime): TimeSeries[Float] = xs.slice(sd, ed)
 
